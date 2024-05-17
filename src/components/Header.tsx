72cd747dc0fc73406e35e262e273ad776d3cb405
Yaswanth <ykandimalla@gmail.com>
2024-05-18 01:30:03 +0530

    made header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import { StyledHeader } from "../GlobalStyles";
+import { Games, useGlobalStore } from "../store/useGlobalStore";
 import Info from "./Info";
 
 const Header = () => {
+  const [updateSelectionGame] = useGlobalStore((state) => [
+    state.updateSelectionGame,
+  ]);
+
   return (
     <StyledHeader>
-      <h3>JoyRover</h3>
+      <h3 onClick={() => updateSelectionGame(Games.GAME_SELECTION)}>
+        JoyRover
+      </h3>
       <Info />
     </StyledHeader>
   );
--- src/games/MemoryGame/index.tsx
+++ src/games/MemoryGame/index.tsx
@@ -1,15 +1,11 @@
-import { IoMdArrowRoundBack } from "react-icons/io";
 import { Container, StyledTitle } from "../../GlobalStyles";
-import { Games, useGlobalStore } from "../../store/useGlobalStore";
 import GameOver from "./components/GameOver";
 import InGame from "./components/InGame";
 import LevelSelection from "./components/LevelSelection";
 import { GameModes, useMemoryGameStore } from "./store/useMemoryGameStore";
 function MemoryGame() {
   const [mode] = useMemoryGameStore((state) => [state.mode]);
-  const [updateSelectionGame] = useGlobalStore((state) => [
-    state.updateSelectionGame,
-  ]);
+
   const renderComponent = {
     [GameModes.LEVEL]: <LevelSelection />,
     [GameModes.INGAME]: <InGame />,
@@ -18,12 +14,7 @@ function MemoryGame() {
 
   return (
     <Container>
-      <StyledTitle>
-        <div onClick={() => updateSelectionGame(Games.GAME_SELECTION)}>
-          <IoMdArrowRoundBack />
-        </div>
-        Memory Game
-      </StyledTitle>
+      <StyledTitle>Memory Game</StyledTitle>
       {renderComponent[mode]}
     </Container>
   );
